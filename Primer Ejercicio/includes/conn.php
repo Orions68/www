a5756f9304fc3114180e1048a81608574603896c
Orions68 <orions68@gmail.com>
2023-02-09 17:02:14 +0000

    conn

--- Primer Ejercicio/includes/conn.php
+++ Primer Ejercicio/includes/conn.php
@@ -3,7 +3,7 @@ session_start();
 ob_start();
 try
 {
-	$conn = new PDO('mysql:host=localhost;dbname=firstdb', "root", "Anubis68");
+	$conn = new PDO('mysql:host=localhost;dbname=firstdb', "root", "");
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e)
