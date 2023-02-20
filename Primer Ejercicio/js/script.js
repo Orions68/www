10613cb29e0498178cba98c43b76c26d76ec458c
Orions68 <orions68@gmail.com>
2023-02-20 16:30:43 +0000

    Curso-3

--- Primer Ejercicio/js/script.js
+++ Primer Ejercicio/js/script.js
@@ -2,7 +2,17 @@ function screen()
 {
     let view1 = document.getElementById("view1");
     let view2 = document.getElementById("view2");
+
+    view(view1);
+    if (view2 != null)
+    {
+        view(view2);
+    }
+}
+
+function view(view)
+{
     let height = height;
 
-    
+    view.style.height = height;
 }
