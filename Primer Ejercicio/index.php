8921f4776155d96ebd85e58e7ecfc724419e4740
Orions68 <orions68@gmail.com>
2023-02-20 16:22:40 +0000

    Curso-2

--- Primer Ejercicio/index.php
+++ Primer Ejercicio/index.php
@@ -3,7 +3,22 @@ include "includes/conn.php";
 $title = "Primera Base de Datos";
 include "includes/header.php";
 ?>
-    <h1>Hola Mundo!</h1>
+<section class="container-fluid pt-3">
+    <div class="row">
+        <div class="col-md-1"></div>
+            <div class="col-md-10">
+                <div id="view1">
+                    <br><br><br><br>
+                    <h1>Hola Mundo!</h1>
+                </div>
+                <div id="view2">
+                    <br><br><br><br>
+                    <h1>Segunda Vista</h1>
+                </div>
+            </div>
+        <div class="col-md-1"></div>
+    </div>
+</section>
 <?php
 include "includes/footer.html";
 ?>
