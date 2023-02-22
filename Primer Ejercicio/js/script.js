e86bab905d47b842db8f6637ee2bd6456a1f0b0a
Orions68 <orions68@gmail.com>
2023-02-22 17:05:59 +0000

    Updating

--- Primer Ejercicio/js/script.js
+++ Primer Ejercicio/js/script.js
@@ -3,17 +3,19 @@ function screen()
     let view1 = document.getElementById("view1");
     let view2 = document.getElementById("view2");
 
-    view(view1);
+    view(view1, view1.innerHeight);
     if (view2 != null)
     {
-        view(view2);
+        view(view2, view2.innerHeight);
     }
 }
 
-function view(views)
+function view(views, heightView)
 {
     let height = window.innerHeight;
 
-    views.style.height = height + "px";
-    console.log("El tamaño vertical de la pantalla es: " + height);
+    if (heightView < height)
+    {
+        views.style.height = height + "px";
+    }
 }
